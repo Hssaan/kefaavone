f4ffa54355c91ed45a9396469fcfd666e3b25377
Hassan Almalki <halmalki.ste@outlook.com>
2020-12-24 04:24:24 +0300

    fix bugs

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -26,13 +26,13 @@
                                 <td>{{$user->email}}</td>
                                 <td>@if($user->isadmin) مدير @else عميل @endif</td>
                                 <td>
-                                    @if(auth()->user()->uuid != $user->uuid)
-                                    <form action="{{ route('user.delete',$user->uuid) }}" method="POST">
+                                    @if(auth()->user()->id != $user->id)
+                                    <form action="{{ route('user.delete',$user->id) }}" method="POST">
                                         {{ csrf_field() }}
                                         <button type="submit" class="btn btn-danger">حذف</button>
                                     </form>
                                     @endif
-                                    <a href="{{ route('user.edit',$user->uuid) }}" type="button" class="btn btn-warning">تعديل</a>
+                                    <a href="{{ route('user.edit',$user->id) }}" type="button" class="btn btn-warning">تعديل</a>
                                 </td>
                             </tr>
                             @endforeach
